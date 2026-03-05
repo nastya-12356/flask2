1aa842a8d5d461cdd828b3eb175d09b47bff4570
Diivika <diivika2@yandex.ru>
2026-03-05 17:04:52 +0300

    update all

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import os
 
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def name():
-    return "Миссия Колонизация Марса"
-
-
-@app.route('/index')
-def index():
-    return "И на Марсе будут яблони цвести!"
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+answers = {
+    'title': 'Анкета',
+    'surname': 'Watny',
+    'name': 'Mark',
+    'education': 'выше среднего',
+    'profession': 'штурман марсохода',
+    'sex': 'male',
+    'motivation': 'Всегда мечтал застрять на Марсе!',
+    'ready': 'True'
+}
 
 
 @app.route('/promotion')
@@ -328,6 +329,55 @@ def carousel():
                       </body>
                     </html>"""
 
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    param = {}
+    param['title'] = title
+    return render_template('index.html', **param)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    param = {}
+    param['profession'] = prof
+    return render_template('prof.html', **param)
+
+
+@app.route('/list_prof/<listing>')
+def spisok(listing):
+    param = {}
+    param['how'] = listing
+    return render_template('prof_listing.html', **param)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    return render_template('auto_answer.html', **answers)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        print(request.form['id_astronaut'], '')
+        print(request.form['password_astronaut'], '')
+        print(request.form['id_captain'], '')
+        print(request.form['password_captain'], '')
+
+        return "Доступ разрешен"
+
+
+@app.route('/distribution')
+def distribution():
+    return render_template('auto_answer.html', **answers)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
