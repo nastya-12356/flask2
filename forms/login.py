e890adb2cc9feffc3568685ea131494edb003581
nastya-12356 <you@example.com>
2026-03-23 17:20:08 +0300

    shjk

---
+++ forms/login.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, IntegerField, BooleanField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+pass
+
